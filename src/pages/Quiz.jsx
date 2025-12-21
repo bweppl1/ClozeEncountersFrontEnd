191dd01538e49551bb1389914af0768e7d7e48d9
Brent Weppler <brentweppler@gmail.com>
2025-12-21 10:47:57 -0600

    building out API calls2

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,5 +1,35 @@
+import { get_random_cloze } from "../services/quiz";
+import { useState, useEffect } from "react";
+
 const Quiz = () => {
-  return <div className="flex-1 max-w-5xl mx-auto">Quiz</div>;
+  const [cloze, setCloze] = useState("");
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState("");
+  const [spanish, setSpanish] = useState("");
+
+  useEffect(() => {
+    const fetchClozeData = async () => {
+      const cloze_data = await get_random_cloze();
+      console.log(cloze_data);
+      const random_index = Math.floor(
+        Math.random() * cloze_data.sentences.length,
+      );
+      const random_sentence = cloze_data.sentences[random_index];
+
+      setWord(cloze_data.word);
+      setSpanish(random_sentence.spanish);
+      setTranslation(random_sentence.english);
+    };
+    fetchClozeData();
+  }, []);
+
+  return (
+    <div className="flex-1 max-w-5xl mx-auto">
+      <div>{word}</div>
+      <div>{spanish}</div>
+      <div>{translation}</div>
+    </div>
+  );
 };
 
 export default Quiz;
--- src/services/quiz.js
+++ src/services/quiz.js
@@ -3,10 +3,10 @@ import axios from "axios";
 
 const API_BASE = "http://localhost:8000";
 
-const get_random_cloze = async () => {
+export const get_random_cloze = async () => {
   try {
-    const response = await axios.get(`${API_BASE}/items/`);
-    setItems(response.data);
+    const response = await axios.get(`${API_BASE}/random_cloze/`);
+    return response.data;
   } catch (error) {
     console.error("Error fetching random cloze:", error);
   }
