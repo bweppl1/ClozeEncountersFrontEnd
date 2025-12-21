673866d5f65b382aa304108a1ceb65be7cfd8d80
brent weppler <brentweppler@gmail.com>
2025-12-21 12:15:27 -0600

    1st full successful data pull

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -4,30 +4,35 @@ import { useState, useEffect } from "react";
 const Quiz = () => {
   const [cloze, setCloze] = useState("");
   const [word, setWord] = useState("");
-  const [translation, setTranslation] = useState("");
+  const [wordId, setWordId] = useState(null);
+  const [english, setEnglish] = useState("");
   const [spanish, setSpanish] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [chosenWords, setChosenWords] = useState([]);
 
   useEffect(() => {
     const fetchClozeData = async () => {
       const cloze_data = await get_random_cloze();
       console.log(cloze_data);
-      const random_index = Math.floor(
-        Math.random() * cloze_data.sentences.length,
-      );
-      const random_sentence = cloze_data.sentences[random_index];
 
-      setWord(cloze_data.word);
-      setSpanish(random_sentence.spanish);
-      setTranslation(random_sentence.english);
+      setCloze(cloze_data["cloze"]);
+      setWord(cloze_data["word"]);
+      setWordId(cloze_data["word_id"]);
+      setEnglish(cloze_data["english"]);
+      setSpanish(cloze_data["spanish"]);
+      setAnswer(cloze_data["answer"]);
     };
     fetchClozeData();
   }, []);
 
   return (
     <div className="flex-1 max-w-5xl mx-auto">
+      <div>{wordId}</div>
       <div>{word}</div>
+      <div>{answer}</div>
+      <div>{cloze}</div>
       <div>{spanish}</div>
-      <div>{translation}</div>
+      <div>{english}</div>
     </div>
   );
 };
